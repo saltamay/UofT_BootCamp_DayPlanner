f21681ce972a22d6f2a0ddd32c5533c29dad6d65
Sal Tamay <sal.tamay@outlook.com>
2019-10-25 18:16:02 -0400

    Pull and display current date

--- js/app.js
+++ js/app.js
@@ -1,10 +1,46 @@
 /**
  * Get the current date and display it
  */
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// Get day
+let dayIndex = moment().day();
+let day = days[dayIndex];
+// Get month
+let monthIndex = moment().month();
+let month = months[monthIndex];
+// Get date
+let date = moment().date();
+switch (date) {
+  case 1:
+    date += 'st';
+    break;
+  case 2:
+    date += 'nd';
+    break;
+  case 3:
+    date += 'rd';
+    break;
+  case 21:
+    date += 'st';
+  case 22:
+    date += 'nd';
+    break;
+  case 23:
+    date += 'rd';
+    break;
+  default:
+    date += 'th';
+    break;
+}
+
+let today = `${day}, ${month} ${date}`;
+$('#currentDay').text(today);
 
 /**
  * Display timeblocks
  */
+
 // Create a div with class of row
 const row = $('<div>');
 row.addClass('row time-block');
